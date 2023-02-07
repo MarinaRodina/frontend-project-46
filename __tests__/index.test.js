b555eefa806b4d3db58f03af740b1619905e6cd7
marina rodina <mrodina1990@yandex.ru>
2023-02-07 13:55:53 +0500

    fix data file1.yml and file2.yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,10 +10,10 @@ const readFile = (filepath) => readFileSync(filepath, 'utf8');
 
 test('genDiff - compare JSON files', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))
-    .toEqual(readFile(getFixturePath('expected_files_JSON.txt')));
+    .toEqual(readFile(getFixturePath('expected_files.txt')));
 });
 
 test('genDiff - compare YML files', () => {
   expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')))
-    .toEqual(readFile(getFixturePath('expected_files_yml.txt')));
+    .toEqual(readFile(getFixturePath('expected_files.txt')));
 });
