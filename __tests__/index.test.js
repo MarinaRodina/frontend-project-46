9beb9a72a06886dca7293342021ca8368456da88
marina rodina <mrodina1990@yandex.ru>
2023-02-13 19:43:01 +0500

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,51 +8,44 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filepath) => readFileSync(filepath, 'utf8');
 
-test('genDiff - compare JSON files', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))
-    .toEqual(readFile(getFixturePath('expected_files.txt')));
-});
+const tests = [
+  {
+    file1: 'file1.json', file2: 'file2.json', expected: 'expected_files.txt',
+  },
+  {
+    file1: 'file1.yml', file2: 'file2.yml', expected: 'expected_files.txt',
+  },
+  {
+    file1: 'file3.json', file2: 'file4.json', expected: 'expected_files_stylish.txt',
+  },
+  {
+    file1: 'file3.yaml', file2: 'file4.yaml', expected: 'expected_files_stylish.txt',
+  },
+  {
+    file1: 'file3.json', file2: 'file4.json', expected: 'expected_files_stylish.txt', format: 'stylish',
+  },
+  {
+    file1: 'file3.yaml', file2: 'file4.yaml', expected: 'expected_files_stylish.txt', format: 'stylish',
+  },
+  {
+    file1: 'file3.json', file2: 'file4.json', expected: 'expected_files_plain.txt', format: 'plain',
+  },
+  {
+    file1: 'file3.yaml', file2: 'file4.yaml', expected: 'expected_files_plain.txt', format: 'plain',
+  },
+  {
+    file1: 'file3.json', file2: 'file4.json', expected: 'expected_files_json.txt', format: 'json',
+  },
+  {
+    file1: 'file3.yaml', file2: 'file4.yaml', expected: 'expected_files_json.txt', format: 'json',
+  },
+];
 
-test('genDiff - compare YML files', () => {
-  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')))
-    .toEqual(readFile(getFixturePath('expected_files.txt')));
-});
-
-test('genDiff - nested JSON files stylish', () => {
-  expect(genDiff(getFixturePath('file3.json'), getFixturePath('file4.json'), 'stylish'))
-    .toEqual(readFile(getFixturePath('expected_files_stylish.txt')));
-});
-
-test('genDiff - nested YAML files stylish', () => {
-  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yaml'), 'stylish'))
-    .toEqual(readFile(getFixturePath('expected_files_stylish.txt')));
-});
-
-test('genDiff - nested JSON files default format', () => {
-  expect(genDiff(getFixturePath('file3.json'), getFixturePath('file4.json')))
-    .toEqual(readFile(getFixturePath('expected_files_stylish.txt')));
-});
-test('genDiff - nested YAML files default format', () => {
-  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yaml')))
-    .toEqual(readFile(getFixturePath('expected_files_stylish.txt')));
-});
-
-test('genDiff - nested JSON files plain', () => {
-  expect(genDiff(getFixturePath('file3.json'), getFixturePath('file4.json'), 'plain'))
-    .toEqual(readFile(getFixturePath('expected_files_plain.txt')));
-});
-
-test('genDiff - nested YML files plain', () => {
-  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yaml'), 'plain'))
-    .toEqual(readFile(getFixturePath('expected_files_plain.txt')));
-});
-
-test('genDiff - nested JSON files json', () => {
-  expect(genDiff(getFixturePath('file3.json'), getFixturePath('file4.json'), 'json'))
-    .toEqual(readFile(getFixturePath('expected_files_json.txt')));
-});
-
-test('genDiff - nested YML files json', () => {
-  expect(genDiff(getFixturePath('file3.yaml'), getFixturePath('file4.yaml'), 'json'))
-    .toEqual(readFile(getFixturePath('expected_files_json.txt')));
+test.each(tests)('gendiff %s', ({
+  file1, file2, expected, format,
+}) => {
+  const filepath1 = getFixturePath(file1);
+  const filepath2 = getFixturePath(file2);
+  const expectedResult = readFile(getFixturePath(expected));
+  expect(genDiff(filepath1, filepath2, format)).toEqual(expectedResult);
 });
