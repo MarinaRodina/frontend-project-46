82aba0a01d1d483450e32f71f40c593666bb2814
marina rodina <mrodina1990@yandex.ru>
2023-02-13 15:15:17 +0500

    fix parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,17 @@
 import yaml from 'js-yaml';
 
 const parsers = (data, format) => {
-  if (format.toLowerCase() === 'json') {
-    return JSON.parse(data);
-  } if (format.toLowerCase() === 'yml' || format.toLowerCase() === 'yaml') {
-    return yaml.load(data);
-  } return `Unknown format: ${format}`;
+  const formatToLowerCase = format.toLowerCase();
+  switch (formatToLowerCase) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+      return yaml.load(data);
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      return `Unknown format: ${format}`;
+  }
 };
 
 export default parsers;
