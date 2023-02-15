890dca3ecf66bd4f5a640315efc3fd88555362b6
marina rodina <mrodina1990@yandex.ru>
2023-02-15 18:04:52 +0500

    fix index.js

--- src/index.js
+++ src/index.js
@@ -13,10 +13,10 @@ const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = readFile(getAbsolutePath(filepath1));
   const data2 = readFile(getAbsolutePath(filepath2));
 
-  const obj1 = parsers(data1, getExtension(filepath1));
-  const obj2 = parsers(data2, getExtension(filepath2));
+  const parsedData1 = parsers(data1, getExtension(filepath1));
+  const parsedData2 = parsers(data2, getExtension(filepath2));
 
-  const diffTree = buildTree(obj1, obj2);
+  const diffTree = buildTree(parsedData1, parsedData2);
 
   return format(diffTree, formatName);
 };
