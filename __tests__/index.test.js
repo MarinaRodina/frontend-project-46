93fd727b699ae51fbbe80101dd48afcc3ff87f0c
marina rodina <mrodina1990@yandex.ru>
2023-02-16 20:42:17 +0500

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,36 +10,36 @@ const readFile = (filepath) => readFileSync(filepath, 'utf8');
 
 const tests = [
   {
-    file1: 'file3.json', file2: 'file4.json', expected: 'expected_files_stylish.txt',
+    filename1: 'file3.json', filename2: 'file4.json', expected: 'expected_files_stylish.txt',
   },
   {
-    file1: 'file3.yaml', file2: 'file4.yaml', expected: 'expected_files_stylish.txt',
+    filename1: 'file3.yaml', filename2: 'file4.yaml', expected: 'expected_files_stylish.txt',
   },
   {
-    file1: 'file3.json', file2: 'file4.json', expected: 'expected_files_stylish.txt', format: 'stylish',
+    filename1: 'file3.json', filename2: 'file4.json', expected: 'expected_files_stylish.txt', format: 'stylish',
   },
   {
-    file1: 'file3.yaml', file2: 'file4.yaml', expected: 'expected_files_stylish.txt', format: 'stylish',
+    filename1: 'file3.yaml', filename2: 'file4.yaml', expected: 'expected_files_stylish.txt', format: 'stylish',
   },
   {
-    file1: 'file3.json', file2: 'file4.json', expected: 'expected_files_plain.txt', format: 'plain',
+    filename1: 'file3.json', filename2: 'file4.json', expected: 'expected_files_plain.txt', format: 'plain',
   },
   {
-    file1: 'file3.yaml', file2: 'file4.yaml', expected: 'expected_files_plain.txt', format: 'plain',
+    filename1: 'file3.yaml', filename2: 'file4.yaml', expected: 'expected_files_plain.txt', format: 'plain',
   },
   {
-    file1: 'file3.json', file2: 'file4.json', expected: 'expected_files_json.txt', format: 'json',
+    filename1: 'file3.json', filename2: 'file4.json', expected: 'expected_files_json.txt', format: 'json',
   },
   {
-    file1: 'file3.yaml', file2: 'file4.yaml', expected: 'expected_files_json.txt', format: 'json',
+    filename1: 'file3.yaml', filename2: 'file4.yaml', expected: 'expected_files_json.txt', format: 'json',
   },
 ];
 
 test.each(tests)('gendiff %s', ({
-  file1, file2, expected, format,
+  filename1, filename2, expected, format,
 }) => {
-  const filepath1 = getFixturePath(file1);
-  const filepath2 = getFixturePath(file2);
+  const filepath1 = getFixturePath(filename1);
+  const filepath2 = getFixturePath(filename2);
   const expectedResult = readFile(getFixturePath(expected));
   expect(genDiff(filepath1, filepath2, format)).toEqual(expectedResult);
 });
